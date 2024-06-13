0158e68d0e92e710dd9dbea2624daace0d53e258
Mathi-MS <sammathi17@gmail.com>
2024-06-13 21:45:12 +0530

    first commit

---
+++ Assests/script.js
@@ -0,0 +1,48 @@
+var navigateone = document.querySelector(".navigate__one")
+var navigatetwo = document.querySelector(".navigate__two")
+var navigatethree = document.querySelector(".navigate__three")
+var bannerone = document.querySelector(".banner__one")
+var overlay = document.querySelector(".overlay_one")
+var bannerimg = document.querySelector(".bannerimg")
+
+
+navigatetwo.addEventListener("click",()=>{
+    bannerone.style.backgroundColor=("#ff677d")
+    overlay.style.backgroundColor=("#fcb3beec")
+    bannerimg.setAttribute("src","Assests/images/banner-1.png")
+    bannerimg.classList.add("anime")
+    navigatetwo.style.opacity=(".5")
+    navigateone.style.opacity=("1")
+    navigatethree.style.opacity=("1")
+    setTimeout(function(){
+        bannerimg.classList.remove("anime")
+    }, 1000);
+})
+
+navigateone.addEventListener("click",()=>{
+    bannerone.style.backgroundColor=("#7B4B41")
+    overlay.style.backgroundColor=("#ce9184")
+    bannerimg.setAttribute("src","Assests/images/banner-2.png")
+    bannerimg.classList.add("anime")
+    navigateone.style.opacity=(".5")
+    navigatetwo.style.opacity=("1")
+    navigatethree.style.opacity=("1")
+    setTimeout(function(){
+        bannerimg.classList.remove("anime")
+    }, 1000);
+})
+
+navigatethree.addEventListener("click",()=>{
+    bannerone.style.backgroundColor=("#446F25")
+    overlay.style.backgroundColor=("#498b19")
+    bannerimg.setAttribute("src","Assests/images/banner-3.png")
+    bannerimg.classList.add("anime")
+    navigatethree.style.opacity=(".5")
+    navigateone.style.opacity=("1")
+    navigatetwo.style.opacity=("1")
+    setTimeout(function(){
+        bannerimg.classList.remove("anime")
+    }, 1000);
+
+})
+
